f3ab78a35e8efe9f25c4c0c0650cd7026bb3673e
Nelson Rodrigues <nbr@Nelsons-MBP.home>
2016-01-29 23:02:45 +0000

    Move semantics

--- cpp/ModernCpp/compilation/main.cpp
+++ cpp/ModernCpp/compilation/main.cpp
@@ -85,7 +85,60 @@ class Vector
     T _points[Size];
 };
 
-       	
+
+        
+//===================================================
+// Move Semantics
+//===================================================
+
+template <typename T>
+class Buffer
+{
+    std::string          _name;
+    size_t               _size;
+    std::unique_ptr<T[]> _buffer;
+    
+public:
+    // constructor
+    Buffer(const std::string& name = "", size_t size = 16):
+    _name(name),
+    _size(size),
+    _buffer(size? new T[size] : nullptr)
+    {}
+    
+    // copy constructor
+    Buffer(const Buffer& copy):
+    _name(copy._name),
+    _size(copy._size),
+    _buffer(copy._size? new T[copy._size] : nullptr)
+    {
+        T* source = copy._buffer.get();
+        T* dest = _buffer.get();
+        std::copy(source, source + copy._size, dest);
+    }
+    
+    // copy assignment operator
+    Buffer& operator=(Buffer copy)
+    {
+        swap(*this, copy);
+        return *this;
+    }
+    
+    // move constructor
+    Buffer(Buffer&& temp):Buffer()
+    {
+        swap(*this, temp);
+    }
+    
+    friend void swap(Buffer& first, Buffer& second) noexcept
+    {
+        using std::swap;
+        swap(first._name  , second._name);
+        swap(first._size  , second._size);
+        swap(first._buffer, second._buffer);
+    }
+};
+
 
 int main(int argc, const char * argv[]) {
     
@@ -248,8 +301,14 @@ int main(int argc, const char * argv[]) {
     Vector<double, 2> a2;
     
     
-
+    //===================================================
+    // Move Semantics
+    //===================================================
     
+
+    Buffer<int> b1;
+    Buffer<int> b2("buf2", 64);
+    Buffer<int> b3 = b2;
     
     return 0;
 }
